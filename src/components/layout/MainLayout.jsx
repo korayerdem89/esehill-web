c4a75e4bd244837636ff8d6d50c1c09b948d6044
Koray Erdem KILIÇ <korayerdem89@gmail.com>
2024-05-27 18:21:56 +0300

    services screen added, fixed layout and structure

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import Header2 from "../header/Header";
 import Footer2 from "../footer/Footer2";
 import Breadcrumb from "../common/Breadcrumb";
-import { useRouter } from "next/router";
 
 const MainLayout = ({ children }) => {
-  const route = useRouter();
-  const path = route.pathname;
   return (
     <>
       <Header2 />
